84199131321058b441070414e60b67205a0f5484
HatusP <hpellegr@espol.edu.ec>
2021-11-14 16:40:35 -0500

    Agregado diccionario con palabras reservadas

--- lexer.py
+++ lexer.py
@@ -1,3 +1,40 @@
 import ply.lex as lex
 
-print("Hola Mundo")
+# Aporte Hatus Pellegrini
+reserved = {
+    'if': 'IF',
+    'and': 'AND',
+    'alias': 'ALIAS',
+    'for': 'FOR',
+    'begin': 'BEGIN',
+    'break': 'BREAK',
+    'case': 'CASE',
+    'class': 'CLASS',
+    'def': 'DEF',
+    'defined?': 'DEFINED?',
+    'do': 'DO',
+    'else': 'ELSE',
+    'elsif': 'ELSIF',
+    'end': 'END',
+    'ensure': 'ENSURE',
+    'false': 'FALSE',
+    'module': 'MODULE',
+    'next': 'NEXT',
+    'nil': 'NIL',
+    'not': 'NOT',
+    'or': 'OR',
+    'redo': 'REDO',
+    'rescue': 'RESCUE',
+    'retry': 'RETRY',
+    'return': 'RETURN',
+    'self': 'SELF',
+    'super': 'SUPER',
+    'then': 'THEN',
+    'true': 'TRUE',
+    'undef': 'UNDEF',
+    'unles': 'UNLES',
+    'until': 'UNTIL',
+    'when': 'WHEN',
+    'while': 'WHILE',
+    'yield': 'YIELD'
+}
