9a4cfbc92977df069e8f2998d734e6b83367651e
NahinMontoya <73256114+NahinMontoya@users.noreply.github.com>
2021-11-21 16:34:56 -0500

    Se agrego el comienzo de sintactico

---
+++ sintactico.py
@@ -0,0 +1,17 @@
+import ply.yacc as yacc
+from lexer import tokens
+def p_sentencias(p):
+     'sentencias: VARIABLE'
+
+def p_error(p):
+    print("Error sintactico")
+
+parser = yacc.yacc()
+while True:
+    try:
+        s=input('calc>')
+    except EOFError:
+        break
+    if not s: continue
+    result =parser.parse(s)
+    print(result)
