701d5d288c1480e9e581add2adfa38ded9373230
HatusP <hpellegr@espol.edu.ec>
2021-11-21 22:34:34 -0500

    Agregado aportes individuales

--- sintactico.py
+++ sintactico.py
@@ -1,43 +1,62 @@
 import ply.yacc as yacc
 from lexer import tokens
+
+
 # aporte de Josue Montoya Ortiz
 def p_sentecias(p):
     '''sentencias : estructurasControl
                 | declaracion
                 '''
+
+
 # aporte de Josue Montoya Ortiz
 def p_estructurasControl(p):
     '''estructurasControl : estrucIf
                             | estrucElse
                             | estrucWhile
                             '''
-
+# aporte de Josue Montoya Ortiz
+def p_operadorMat(p):
+    '''operadorMat : IGUAL
+                | PLUS
+                | MINUS
+                | TIMES
+                '''
 # aporte de Josue Montoya Ortiz
 def p_operadorLog(p):
     '''operadorLog : AND
                  | OR
                 '''
+
+
 # aporte de Josue Montoya Ortiz
 def p_estrucIf(p):
     '''estrucIf : IF logica cuerpo END
                 | IF logica cuerpo estrucElse
                 '''
 
+
 # aporte de Moisés Atupaña
 def p_logica(p):
     '''logica : condicion
                 | condicion operadorLog  logica
                 '''
+
+
 # aporte de Moisés Atupaña
 def p_condicion(p):
     '''condicion : comparador COMPARACION comparador
                     | boolean
                     '''
+
+
 # aporte de Moisés Atupaña
 def p_comparador(p):
     '''comparador : VARIABLE
                     | primitivo
                     '''
+
+
 
 # aporte de Josue Montoya Ortiz y Moisés Atupaña
 def p_estrucElse(p):
@@ -45,10 +64,12 @@ def p_estrucElse(p):
                     | ELSE logica cuerpo END
                     '''
 
+
 # aporte de Josue Montoya Ortiz
 def p_estrucWhile(p):
     ''' estrucWhile : WHILE logica cuerpo END
                     '''
+
 
 # aporte de Josue Montoya Ortiz
 def p_cuerpo(p):
@@ -56,6 +77,7 @@ def p_cuerpo(p):
                 | declaracion cuerpo
                 | cuerpo RETURN retornos
                 '''
+
 
 # aporte de Josue Montoya Ortiz
 def p_retornos(p):
@@ -65,15 +87,8 @@ def p_retornos(p):
                 | asignacion
                 '''
 
-# aporte de Josue Montoya Ortiz
-def p_operadorMat(p):
-    '''operadorMat : PLUS
-                | MINUS
-                | TIMES
-                | DIVIDE
-                '''
 
-#aporte de Josue Montoya Ortiz
+# aporte de Josue Montoya Ortiz
 def p_declaracion(p):
     '''declaracion : VARIABLE IGUAL asignacion
                     | VARIABLE IGUAL asignacion declaracion
@@ -86,21 +101,28 @@ def p_opcion(p):
                 | NUMBER
                 '''
 
+
+# aporte Hatus Pellegrini
 def p_declaracion_funcion(p):
     'declaracion : DEF VARIABLE LPAREN params RPAREN cuerpo END'
 
 
+# aporte Hatus Pellegrini
 def p_params(p):
     '''params : rubyParams
                | rubyParams COMA params
                   '''
     pass
 
+
+# aporte Hatus Pellegrini
 def p_rubyParams(p):
     '''rubyParams : VARIABLE
                     | boolean
                     | NUMBER'''
 
+
+# aporte Hatus Pellegrini
 def p_asignacion_primitivo(p):
     'asignacion : primitivo'
 
@@ -117,6 +139,7 @@ def p_asignacion_fichero(p):
     'asignacion : FICHERO'
 
 
+# aporte Hatus Pellegrini
 def p_asignacion_expresion(p):
     'asignacion : expresion'
 
@@ -124,54 +147,70 @@ def p_asignacion_expresion(p):
 def p_asignacion_string(p):
     'asignacion : STRING'
 
+
+# aporte Hatus Pellegrini
 def p_expresion_suma(p):
     'expresion : NUMBER PLUS NUMBER'
 
 
+# aporte Hatus Pellegrini
 def p_expresion_resta(p):
     'expresion : NUMBER MINUS NUMBER'
 
 
+# aporte Hatus Pellegrini
 def p_expresion_term(p):
     'expresion : termino'
 
 
+# aporte Hatus Pellegrini
 def p_termino_multi(p):
     'termino : termino TIMES factor'
 
 
+# aporte Hatus Pellegrini
 def p_termino_div(p):
     'termino : termino DIVIDE factor'
 
 
+# aporte Hatus Pellegrini
 def p_termino_factor(p):
     'termino : factor'
 
+
+# aporte Hatus Pellegrini
 def p_factor_number(p):
     'factor : NUMBER'
 
+
+# aporte Hatus Pellegrini
 def p_factor_expresion(p):
     'factor : LPAREN expresion RPAREN'
 
 
+# aporte Hatus Pellegrini
 def p_primitivo_flotante(p):
     'primitivo : NUMBER PUNTO NUMBER'
 
+
+# aporte Hatus Pellegrini
 def p_primitivo_number(p):
     'primitivo : NUMBER'
 
 
+# aporte Hatus Pellegrini
 def p_primitivo_booleano(p):
     'primitivo : boolean'
 
 
-
+# aporte Hatus Pellegrini
 def p_boolean(p):
     '''boolean : TRUE
              | FALSE
         '''
 
-#falta interpolacion
+
+# falta interpoalcion
 
 def p_error(p):
     print("Error sintactico")
